8e7708a9e17fc66c54e463f428e15d935dfb7594
Zhadyra Oralkhanova <z.oralkhanova@napoleongames.be>
2022-05-09 18:56:26 +0600

    feat: add adaptive styling for different screens

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@
         }
     };
 }());
+
+//Burger handler
+(function(){
+    const burger_item = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menu_close_item = document.querySelector('.header_nav_close');
+    burger_item.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menu_close_item.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
+}());
